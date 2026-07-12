901069ec033ce3e4938bc49b45693de5e4df3b06
Mxkarthik <maxwhitie1234@gmail.com>
2026-07-12 12:27:51 +0530

    CRUD Operations Implimented for Projects

--- backend/controllers/projectController.js
+++ backend/controllers/projectController.js
@@ -48,9 +48,65 @@ const getProjectById = async (req , res) => {
     }
 }
 
+const updateProjectById = async (req, res) => {
+    try {
+
+        const { id } = req.params;
+
+        const updatedProject = await Project.findByIdAndUpdate(
+            id,
+            req.body,
+            {
+                new: true,
+                runValidators: true,
+            }
+        );
+
+        if (!updatedProject) {
+            return res.status(404).json({
+                message: "Project not found",
+            });
+        }
+
+        res.status(200).json(updatedProject);
+
+    } catch (error) {
+
+        res.status(500).json({
+            message: error.message,
+        });
+
+    }
+};
+
+const deleteProjectById = async (req, res) => {
+    try {
+
+        const { id } = req.params;
+
+        const deletedProject = await Project.findByIdAndDelete(id);
+
+        if (!deletedProject) {
+            return res.status(404).json({
+                message: "Project not found",
+            });
+        }
+
+        res.status(200).json(deletedProject);
+
+    } catch (error) {
+
+        res.status(500).json({
+            message: error.message,
+        });
+
+    }
+};
+
 module.exports = {
     createProject,
     getAllProjects,
     getProjectById,
-
+    updateProjectById,
+    deleteProjectById,
 };
--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express')
 
 const router = express.Router();
-const {createProject , getAllProjects, getProjectById} = require("../controllers/projectController")
+const {createProject , getAllProjects, getProjectById , updateProjectById , deleteProjectById} = require("../controllers/projectController")
 
 
 router.post("/", createProject);
 router.get("/", getAllProjects);
 router.get("/:id",getProjectById);
+router.put("/:id" , updateProjectById)
+router.delete("/:id", deleteProjectById)
 
 module.exports = router;
