76bde6e4e47446acd6b6f61ea0ee931084d4856e
Mxkarthik <maxwhitie1234@gmail.com>
2026-07-12 11:47:25 +0530

    getProjectById Implimentate Done

--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 
 const router = express.Router();
-const {createProject , getAllProjects} = require("../controllers/projectController")
+const {createProject , getAllProjects, getProjectById} = require("../controllers/projectController")
 
 
 router.post("/", createProject);
 router.get("/", getAllProjects);
+router.get("/:id",getProjectById);
 
 module.exports = router;
