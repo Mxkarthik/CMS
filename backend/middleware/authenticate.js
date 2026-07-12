d803617c8bac8d9a7d12bc9ca26cb151e1454a7f
Mxkarthik <maxwhitie1234@gmail.com>
2026-07-13 01:39:35 +0530

    authenticate Middleware

---
+++ backend/middleware/authenticate.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken")
+const authenticate = (res , req , next) => {
+    try 
+    {
+        const token = req.cookies.token;
+
+        if (!token)
+        {
+            return res.status(401).json({
+                message: "Unauthorized"
+            });
+        }
+        const decoded = jwt.verify(
+            token,
+            process.env.JWT_SECRET
+        );
+
+        req.user = decoded;
+
+        next();
+    }
+    catch (error)
+    {
+        return res.status(401).json({
+            message: "Invalid Token"
+        });
+    }
+};
+
+module.exports = authenticate;
