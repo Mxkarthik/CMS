41041ec0da087cafd0c2084698e5a01b4e8bafad
Mxkarthik <maxwhitie1234@gmail.com>
2026-07-19 21:22:07 +0530

    Implimented muler and streamifier and passed buffer to cloudinary

---
+++ backend/services/uploadServices.js
@@ -0,0 +1,32 @@
+const cloudinary = require("../config/cloudinary");
+const streamifier = require("streamifier");
+
+const uploadImage = async (file) => {
+    return new Promise((resolve, reject) => {
+
+        const stream = cloudinary.uploader.upload_stream(
+            {
+                folder: "portfolio-cms",
+            },
+            (error, result) => {
+
+                if (error) {
+                    return reject(error);
+                }
+
+                resolve({
+                    url: result.secure_url,
+                    publicId: result.public_id,
+                });
+
+            }
+        );
+
+        streamifier
+            .createReadStream(file.buffer)
+            .pipe(stream);
+
+    });
+};
+
+module.exports = uploadImage;
