b59b26668afa3b948bbba0960fbf4a12fe1308af
Mxkarthik <maxwhitie1234@gmail.com>
2026-07-19 21:45:05 +0530

    Taking input of tumbnails and images

--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -1,15 +1,57 @@
-const express = require('express')
-const authenticate = require('../middleware/authenticate')
-
-
+const express = require("express");
 const router = express.Router();
-const {createProject , getAllProjects, getProjectById , updateProjectById , deleteProjectById} = require("../controllers/projectController")
 
+const {
+    createProject,
+    getAllProjects,
+    getProjectById,
+    updateProjectById,
+    deleteProjectById,
+} = require("../controllers/projectController");
 
-router.post("/",authenticate , createProject);
+const authenticate = require("../middleware/authenticate");
+const upload = require("../middleware/upload");
+
+router.post(
+    "/",
+    authenticate,
+    upload.fields([
+        {
+            name: "thumbnail",
+            maxCount: 1,
+        },
+        {
+            name: "images",
+            maxCount: 10,
+        },
+    ]),
+    createProject
+);
+
 router.get("/", getAllProjects);
-router.get("/:id",getProjectById);
-router.put("/:id" , authenticate ,updateProjectById)
-router.delete("/:id", authenticate ,deleteProjectById)
+
+router.get("/:id", getProjectById);
+
+router.put(
+    "/:id",
+    authenticate,
+    upload.fields([
+        {
+            name: "thumbnail",
+            maxCount: 1,
+        },
+        {
+            name: "images",
+            maxCount: 10,
+        },
+    ]),
+    updateProjectById
+);
+
+router.delete(
+    "/:id",
+    authenticate,
+    deleteProjectById
+);
 
 module.exports = router;
